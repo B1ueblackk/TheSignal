ca3efb05a528c5e2f34497fdcafb84e93530f821
Edward <191250195@smail.nju.edu.cn>
2021-04-04 13:33:07 +0800

    1

--- main.a9761.js
+++ main.a9761.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.03a6b.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.cc85b.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
